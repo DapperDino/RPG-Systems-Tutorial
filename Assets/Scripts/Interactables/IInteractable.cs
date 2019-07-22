a9acd3d5b6a5a3270b6cd2a798209008a3e9e7c1
DESKTOP-BRTMTMV\Nathan <natemi1010@gmail.com>
2019-07-22 12:25:54 +0100

    End Of Video 3 - Implemented interaction system

---
+++ Assets/Scripts/Interactables/IInteractable.cs
@@ -0,0 +1,7 @@
+﻿namespace DapperDino.Interactables
+{
+    public interface IInteractable
+    {
+        void Interact();
+    }
+}
---
+++ Assets/Scripts/Interactables/Interactor.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace DapperDino.Interactables
+{
+    public class Interactor : MonoBehaviour
+    {
+        private IInteractable currentInteractable = null;
+
+        private void Update()
+        {
+            CheckForInteraction();
+        }
+
+        private void CheckForInteraction()
+        {
+            if (currentInteractable == null) { return; }
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                currentInteractable.Interact();
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            var interactable = other.GetComponent<IInteractable>();
+
+            if (interactable == null) { return; }
+
+            currentInteractable = interactable;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            var interactable = other.GetComponent<IInteractable>();
+
+            if (interactable == null) { return; }
+
+            if (interactable != currentInteractable) { return; }
+
+            currentInteractable = null;
+        }
+    }
+}
